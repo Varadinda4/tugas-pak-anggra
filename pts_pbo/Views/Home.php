58d41a4167cf7c6943532c52c4e69b02262b55fb
Varadinda4 <78637303+Varadinda4@users.noreply.github.com>
2021-11-15 11:28:30 +0700

    pts_pbo
    Tugas aplikasi UTS Nurvara Adinda Khudzori
    XII RPL 1

---
+++ pts_pbo/Views/Home.php
@@ -0,0 +1,14 @@
+<body>
+
+    <div class="card text-dark bg-info p-2 text-dark bg-opacity-25">
+  <div class="card-header">Project PTS PBO</div>
+  <div class="card-body">
+    <p class="card-text">
+        Nama: Nurvara Adinda Khudzori <br>
+        Kelas: XII RPL-1 <br>
+        No.Absen: 21
+    </p>
+  </div>
+</div>
+
+</body>
